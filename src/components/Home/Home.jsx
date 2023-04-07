fe4e7b0733d9b398d7d0cf5213f160181549d7bb
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-04-07 12:01:21 +0600

    tshirt shown on display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
+
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (tshirt) =>{
+       const newCart = [...cart, tshirt];
+       setCart(newCart)
+    }
+
+    const handleRemoveFromCart = (id) =>{
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining)
+    }
+
+
+
     return (
-        <div>
-          <h2>this is home {tshirts.length}</h2>  
+        <div className='home-container'>
+            <div className="shirts-container">
+                {
+                    tshirts.map(tshirt => <Tshirt tshirt={tshirt} handleAddToCart={handleAddToCart} key={tshirt.id}></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart handleRemoveFromCart={handleRemoveFromCart} cart={cart}></Cart>
+            </div>
+
         </div>
+
     );
 };
 
-export default Home;<h2>this is home </h2>
+export default Home; <h2>this is home </h2>
